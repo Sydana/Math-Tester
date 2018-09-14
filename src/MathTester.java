677697e5534146db1e19cede02b9306ffd5699bc
Sydana Schalk <sydana.schalk.2019@student.lhschools.org>
2018-09-14 14:16:26 -0400

    Progress #1

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,6 +1,33 @@
 public class MathTester {
 
-    public static void main;
+    public static void main(String[] args){
+        String firstword;
+        String secondword;
+        int firstword, secondword, total;
+
+
+
+        firstword = "dog";
+        secondword = "catcher";
+
+        firstword = 25;
+        secondword = 30;
+        total = firstword+secondword;
+        System.out.println(total);
+        total = total+10;
+        System.out.println(total);
+
+
+        System.out.println(firstword+secondword);
+
+
+
+
+
+
+
+
+    };
 
 
 }
